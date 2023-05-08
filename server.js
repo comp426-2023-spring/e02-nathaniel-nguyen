a94d6da283e845832afe1609821274952bf5a89c
Namaniel <nathaniel.nguyen@unc.edu>
2023-05-08 17:03:42 -0400

    Changed some code in server.js. Error with front end functionality.

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 // https://nodejs.org/docs/latest-v18.x/api/module.html#modulecreaterequirefilename
 import { createRequire } from 'node:module';
 import { rps, rpsls } from './lib/rpsls.js';
+
 const require = createRequire(import.meta.url);
 // The above two lines allow us to use ES methods and CJS methods for loading
 // dependencies.
@@ -62,6 +63,57 @@ if (args.debug) {
 const app = express()
 // Set a port for the server to listen on
 const port = args.port || args.p || process.env.PORT || 8080
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// Weird stuff
+var replacementString = '';
+
+app.get('/app/rps/', function(req, res) {
+    let string = JSON.stringify(rps()).replace(/\\/g, replacementString)
+    res.status(200).send(string.substring(1, string.length-1)).end();
+});
+
+app.get('/app/rpsls/', function(req, res) {
+    let string = JSON.stringify(rpsls()).replace(/\\/g, replacementString)
+    res.status(200).send(string.substring(1, string.length-1)).end();
+});
+
+app.get('/app/rps/play', function(req, res) {
+    let string = JSON.stringify(rps(req.query.shot)).replace(/\\/g, replacementString)
+    res.status(200).send(string.substring(1, string.length-1)).end();
+});
+
+app.get('/app/rpsls/play', function(req, res) {
+    let string = JSON.stringify(rpsls(req.query.shot)).replace(/\\/g, replacementString)
+    res.status(200).send(string.substring(1, string.length-1)).end();
+});
+
+app.post('/app/rps/play', function(req, res) {
+    let string = JSON.stringify(rps(req.body.shot)).replace(/\\/g, replacementString)
+    res.status(200).send(string.substring(1, string.length-1)).end();
+});
+
+app.post('/app/rpsls/play', function(req, res) {
+    let string = JSON.stringify(rpsls(req.body.shot)).replace(/\\/g, replacementString)
+    res.status(200).send(string.substring(1, string.length-1)).end();
+});
+
+app.get('/app/rps/play/:shot/', function(req, res) {
+    let string = JSON.stringify(rps(req.params.shot)).replace(/\\/g, replacementString)
+    res.status(200).send(string.substring(1, string.length-1)).end();
+});
+
+app.get('/app/rpsls/play/:shot/', function(req, res) {
+    let string = JSON.stringify(rpsls(req.params.shot)).replace(/\\/g, replacementString)
+    res.status(200).send(string.substring(1, string.length-1)).end();
+});
+
+app.get("/app/", function(req, res) {
+    res.status(200).send("200 OK").end();
+});
+
 // Load app middleware here to serve routes, accept data requests, etc.
 //
 // Create and update access log
@@ -74,73 +126,6 @@ const staticpath = args.stat || args.s || process.env.STATICPATH || path.join(__
 app.use('/', express.static(staticpath))
 // Create app listener
 const server = app.listen(port)
-
-// Root endpoint
-app.get("/app", (req,res) => {
-	res.status(200).send("200 OK");
-});
-
-// Single player rps endpoint
-app.get("/app/rps", (req,res) => {
-	res.json(rps());
-});
-
-// Single player rpsls endpoint
-app.get("/app/rpsls", (req,res) => {
-	res.json(rpsls());
-});
-
-// Multiplayer url encoded request body rps endpoint
-app.get("/app/rps/play", (req,res) => {
-	let shot = req.body.shot;	
-	if (!shot) {
-		shot = req.query.shot;
-	}
-	res.json(rps(shot));
-});
-
-// Multiplayer json request body rps endpoint
-app.post("/app/rps/play", (req,res) => {
-	let shot = req.body.shot;	
-	if (!shot) {
-		shot = req.query.shot;
-	}
-	res.json(rps(shot));
-});
-
-// Multiplayer url encoded request body rpsls endpoint
-app.get("/app/rpsls/play", (req,res) => {
-	let shot = req.body.shot;	
-	if (!shot) {
-		shot = req.query.shot;
-	}
-	res.json(rpsls(shot));
-});
-
-// Multiplayer json request body rpsls endpoint
-app.post("/app/rpsls/play", (req,res) => {
-	let shot = req.body.shot;
-	if (!shot) {
-		shot = req.query.shot;
-	}
-	res.json(rpsls(shot));
-});
-
-// Multiplayer parameter rps endpoint
-app.get("/app/rps/play/:shot", (req,res) => {
-	res.json(rps(req.params.shot));
-});
-
-// Multiplayer parameter rpsls endpoint
-app.get("/app/rpsls/play/:shot", (req,res) => {
-	res.json(rpsls(req.params.shot));
-});
-
-// Defualt route
-app.use(function(req, res){
-	res.status(404).send("404 NOT FOUND");
-});
-
 // Create a log entry on start
 let startlog = new Date().toISOString() + ' HTTP server started on port ' + port + '\n'
 // Debug echo start log entry to STDOUT
